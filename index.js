97a199423934f70af3d8534b469a4560fdbf3572
rogeriofourdev <chatbot.ti.developer@gmail.com>
2022-08-21 08:40:10 -0300

    edição do index: editando comentário sobre import module default

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 
+// TODO: import module default.
 var __importDefault =
   (this && this.__importDefault) ||
   function (mod) {
